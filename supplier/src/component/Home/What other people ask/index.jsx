af2ed9aced307e35ff733b08d8cccf51fa55950d
Ushan Mihiranga <ushanmihiranga2017@gmail.com>
2024-04-13 20:23:28 +0530

    new

--- supplier/src/component/Home/What other people ask/index.jsx
+++ supplier/src/component/Home/What other people ask/index.jsx
@@ -12,12 +12,12 @@ export default class index extends Component {
           <div className="accordion-item">
             <h2 className="accordion-header">
               <button id="dropDownTitle" className="accordion-button  collapsed " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                How does this app work?
+                How does BuySell Nexus ensure secure transactions for buyers and sellers?
               </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <div className="accordion-body" id="bodyTitle">
-                Facial recognition uses technology and biometrics — typically through AI — to identify human faces. It maps facial features from a photograph or video and then compares the information with a database of known faces to find a match. Facial recognition can help verify a person's identity but also raises privacy issues. <span id="learnMore">Learn more </span> 
+              BuySell Nexus employs advanced encryption techniques to secure all transactions, providing a safe environment for both buyers and sellers. Additionally, our platform verifies the identity of users to prevent fraudulent activities.
               </div>
             </div>
           </div>
@@ -25,12 +25,12 @@ export default class index extends Component {
           <div className="accordion-item">
             <h2 className="accordion-header">
               <button id="dropDownTitle" className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Is this app secure?
+              Can sellers manage their inventory easily on BuySell Nexus?
               </button>
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <div  className="accordion-body" id="bodyTitle">
-                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              Yes, BuySell Nexus offers intuitive inventory management tools for sellers, allowing them to easily add, edit, and remove listings. Sellers can also track their inventory levels in real-time and receive notifications for low stock.
               </div>
             </div>
           </div>
@@ -38,12 +38,12 @@ export default class index extends Component {
           <div className="accordion-item">
             <h2 className="accordion-header">
               <button id="dropDownTitle" className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                What type of facial emotion can this app detect?
+              What payment options are available on BuySell Nexus?
               </button>
             </h2>
             <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <div className="accordion-body" id="bodyTitle">
-                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+              BuySell Nexus supports various payment methods including credit/debit cards, PayPal, and bank transfers. Our system ensures seamless payment processing, giving buyers and sellers flexibility in conducting transactions.
               </div>
             </div>
           </div>
@@ -51,12 +51,12 @@ export default class index extends Component {
           <div className="accordion-item">
             <h2 className="accordion-header">
               <button id="dropDownTitle" className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                How long will it take to generate a report?
+              How does BuySell Nexus handle disputes between buyers and sellers?
               </button>
             </h2>
             <div id="collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <div className="accordion-body" id="bodyTitle">
-                Facial recognition uses technology and biometrics — typically through AI — to identify human faces. It maps facial features from a photograph or video and then compares the information with a database of known faces to find a match. Facial recognition can help verify a person's identity but also raises privacy issues. Learn more
+              BuySell Nexus provides a structured dispute resolution process to address any conflicts between buyers and sellers. Our team mediates disputes impartially, striving to achieve fair resolutions that satisfy both parties.
               </div>
             </div>
           </div>
@@ -64,12 +64,12 @@ export default class index extends Component {
           <div className="accordion-item">
             <h2 className="accordion-header">
               <button id="dropDownTitle" className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                How many exams can I create per month?
+              Does BuySell Nexus offer any analytics tools for sellers to track their performance?
               </button>
             </h2>
             <div id="collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <div className="accordion-body" id="bodyTitle">
-                Facial recognition uses technology and biometrics — typically through AI — to identify human faces. It maps facial features from a photograph or video and then compares the information with a database of known faces to find a match. Facial recognition can help verify a person's identity but also raises privacy issues. Learn more
+              Yes, BuySell Nexus provides comprehensive analytics tools for sellers to monitor their performance. Sellers can access detailed reports on sales, customer behavior, and market trends, enabling them to make informed business decisions.
               </div>
             </div>
           </div>
