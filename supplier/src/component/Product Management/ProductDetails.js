dc92bd42e22f754227b76f389fcea25c6f8d93b0
rash0924 <127588221+rash0924@users.noreply.github.com>
2024-04-21 11:17:35 +0530

    update inventory

--- supplier/src/component/Product Management/ProductDetails.js
+++ supplier/src/component/Product Management/ProductDetails.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import NavBar from '../NavBar/NavBar';
-
+import Header from "../Dashboard/Header/Header";
 export default class ProductDetails extends Component {
 
     constructor(props){
@@ -34,37 +34,37 @@ export default class ProductDetails extends Component {
     const {pid, name, type, description, price, quantity, date, sname, contact,  _id } = this.state.product;
     return (
       <div>
-        <NavBar/>
-        <div style={{marginTop:"40px"}} className='container'>
+        <Header />
+        <div className='container' id="productContainer">
           <h4>{name}</h4>
           <hr/>
       <dl className='row'>
-        <dt className='col-sm-3'>Product Tempory ID</dt>
+        <dt className='col-sm-3' id="product">Product Tempory ID</dt>
         <dd className='col-sm-9'>{_id}</dd>
         <br></br><br></br>
-        <dt className='col-sm-3'>Product Code</dt>
+        <dt className='col-sm-3' id="product">Product Code</dt>
         <dd className='col-sm-9'>{pid}</dd>
         <br></br><br></br>
-        <dt className='col-sm-3'>Product Category</dt>
+        <dt className='col-sm-3' id="product">Product Category</dt>
         <dd className='col-sm-9'>{type}</dd>
         <br></br><br></br>
-        <dt className='col-sm-3'>Description</dt>
-        <dd className='col-sm-9'>{description}</dd>
+        <dt className='col-sm-3' id="product">Description</dt>
+        <dd className='col-sm-9' id="details">{description}</dd>
         <br></br><br></br>
-        <dt className='col-sm-3'>Price</dt>
-        <dd className='col-sm-9'>{price}</dd>
+        <dt className='col-sm-3' id="product">Price</dt>
+        <dd className='col-sm-9' id="details">{price}</dd>
         <br></br><br></br>
-        <dt className='col-sm-3'>Quantity</dt>
-        <dd className='col-sm-9'>{quantity}</dd>
+        <dt className='col-sm-3' id="product">Quantity</dt>
+        <dd className='col-sm-9' id="details">{quantity}</dd>
         <br></br><br></br>
-        <dt className='col-sm-3'>Add Date</dt>
-        <dd className='col-sm-9'>{date}</dd>
+        <dt className='col-sm-3' id="product">Add Date</dt>
+        <dd className='col-sm-9' id="details">{date}</dd>
         <br></br><br></br>
-        <dt className='col-sm-3'>Seller Name</dt>
-        <dd className='col-sm-9'>{sname}</dd>
+        <dt className='col-sm-3' id="product">Seller Name</dt>
+        <dd className='col-sm-9' id="details">{sname}</dd>
         <br></br><br></br>
-        <dt className='col-sm-3'>Seller Contact Number</dt>
-        <dd className='col-sm-9'>{contact}</dd>
+        <dt className='col-sm-3' id="product">Seller Contact Number</dt>
+        <dd className='col-sm-9' id="details">{contact}</dd>
         <br></br><br></br>
         <dt className='col-sm-3'>Image</dt>
         <dd className='col-sm-9'>
--- supplier/src/component/Product Management/productList.js
+++ supplier/src/component/Product Management/productList.js
@@ -6,22 +6,22 @@ import "./product.css";
 
 export default class productList extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
-            products:[],
+            products: [],
             productCount: 0,
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.retriveProduct()
     }
 
-    retriveProduct(){
-        axios.get("http://localhost:8070/products").then((res) =>{
-            if(res.data.success){
+    retriveProduct() {
+        axios.get("http://localhost:8070/products").then((res) => {
+            if (res.data.success) {
                 const existingProduct = res.data.existingProduct;
                 this.setState({
                     products: existingProduct,
@@ -31,10 +31,10 @@ export default class productList extends Component {
                 console.log(this.state.products)
             }
         })
-        .catch((error) => console.error(error));
+            .catch((error) => console.error(error));
     }
 
-    onDelete = (id) =>{
+    onDelete = (id) => {
         const isConfirmed = window.confirm('Are you sure you want to delete this product?');
 
         if (isConfirmed) {
@@ -48,100 +48,99 @@ export default class productList extends Component {
         }
     }
 
-    filterData(products, searchKey){
-   
-        const result =  products.filter((product) =>
-           product.name.toLowerCase().includes(searchKey) ||
-           product.pid.toLowerCase().includes(searchKey) ||
-           product.price.toLowerCase().includes(searchKey)
+    filterData(products, searchKey) {
+
+        const result = products.filter((product) =>
+            product.name.toLowerCase().includes(searchKey) ||
+            product.pid.toLowerCase().includes(searchKey) ||
+            product.price.toLowerCase().includes(searchKey)
         )
-      
-        this.setState({products:result})
-      
-      }
 
-      handleSearchArea = (e) =>{
-        const searchKey =  e.currentTarget.value
-     
-        axios.get("http://localhost:8070/products").then((res) =>{
-                 if(res.data.success){
-                     
-                   this.filterData(res.data.existingProduct, searchKey)
-     
-                    
-                 }
-             })
-     }
+        this.setState({ products: result })
+
+    }
+
+    handleSearchArea = (e) => {
+        const searchKey = e.currentTarget.value
+
+        axios.get("http://localhost:8070/products").then((res) => {
+            if (res.data.success) {
+
+                this.filterData(res.data.existingProduct, searchKey)
 
 
-  render() {
-    return (
-      <div>
-       <Header/>
-        <div className='container' id="productContainer">
-         <div className='col-lg-3 mt-2 mb-2'>
-            <input  className="form-control"
-            type='search'
-            placeholder='Search'
-            name="serchQuery"
-            style={{marginLeft:"0px", borderRadius:"20px"}}
-            onChange={this.handleSearchArea}/>
-         </div>
+            }
+        })
+    }
 
-         <div id="productCount" style={{marginBottom:"20px"}}>
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className='container' id="productContainer">
+                    <div className='col-lg-3 mt-2 mb-2'>
+                        <input className="form-control"
+                            type='search'
+                            placeholder='Search'
+                            name="serchQuery"
+                            style={{ marginLeft: "0px", borderRadius: "20px" }}
+                            onChange={this.handleSearchArea} />
+                    </div>
+
+                    <div id="productCount" style={{ marginBottom: "20px" }}>
                         <div className='card-body'>
                             <h5 className='card-title' id="ProductCardTitile" >✅ No. OF PRODUCTS : <span id="cardText"> {this.state.productCount} </span></h5>
                         </div>
                     </div>
-        
 
-        <h2>All Products</h2>
 
-        <button className='btn btn-success' id="addBtn"><a href='add/product' style={{textDecoration:"none", color:"white"}}>
-         <i className='fas fa-plus'></i>&nbsp;Add New</a></button>
-        <br></br>
-         <table className='table table-hover'>
-            <thead>
-                <tr>
-                    <th scope='col'><i className='fas fa-list-ol'></i></th>
-                    <th scope='col'>Product Code</th>
-                    <th scope='col'>Product Name</th>
-                    <th scope='col'>Price</th>
-                    <th scope='col'>Action</th>
-                </tr>
-            </thead>
+                    <h2>All Products</h2>
 
-        <tbody>
-            {this.state.products.map((products, index) =>(
-                <tr key={index}>
-                    <th scope='row'>{index+1}</th>
-                    <td>
-                        <a href= {`/product/${products._id}`} style={{textDecoration:"none"}}>
-                        {products.pid}
-                        </a>
-                        </td>
-                    <td>{products.name}</td>
-                    <td>{products.price}</td>
-                    <td>
-                        <a className='btn' id="editBtn" href={`/editproduct/${products._id}`}>
-                            <i className='fas fa-edit' id="editIcon"></i>&nbsp;
-                        </a>
-                        &nbsp;
-                        <a className='btn' href='# ' id="editDelete"  onClick={() => this.onDelete(products._id)}>
-                            <i className='fas fa-trash-alt' id="DeleteIcon"></i>&nbsp;
-                        </a>
-                        &nbsp;<PdfButton product={products} />
-                    </td>
-                </tr>
-            ))}
-    
-        </tbody>
-         </table>
+                    <button className='btn btn-success' id="addBtn"><a href='add/product' style={{ textDecoration: "none", color: "white" }}>
+                        <i className='fas fa-plus'></i>&nbsp;Add New</a></button>
+                    <br></br>
+                    <table className='table table-hover'>
+                        <thead>
+                            <tr>
+                                <th scope='col'><i className='fas fa-list-ol'></i></th>
+                                <th scope='col'>Product Code</th>
+                                <th scope='col'>Product Name</th>
+                                <th scope='col'>Price</th>
+                                <th scope='col'>Action</th>
+                            </tr>
+                        </thead>
 
-         
-        
-      </div>
-      </div>
-    )
-  }
+                        <tbody>
+                            {this.state.products.map((products, index) => (
+                                <tr key={index}>
+                                    <th scope='row'>{index + 1}</th>
+
+                                    <td>{products.pid}</td>
+                                    <td>{products.name}</td>
+                                    <td>{products.price}</td>
+                                    <td>
+                                        <a className='btn' id="editBtn" href={`/editproduct/${products._id}`}>
+                                            <i className='fas fa-edit' id="editIcon"></i>&nbsp;
+                                        </a>
+                                        &nbsp;
+                                        <a className='btn' href='# ' id="editDelete" onClick={() => this.onDelete(products._id)}>
+                                            <i className='fas fa-trash-alt' id="DeleteIcon"></i>&nbsp;
+                                        </a>
+                                        &nbsp;<PdfButton product={products} />&nbsp;
+
+                                        <a href={`/product/${products._id}`} id="view">VIEW</a>
+                                    </td>
+                                </tr>
+                            ))}
+
+                        </tbody>
+                    </table>
+
+
+
+                </div>
+            </div>
+        )
+    }
 }
