b15561a29d873d16bb38d4b88ca2022e45a8783d
Ridmi Ranasinghe <it22088246@my.sliit.lk>
2024-05-02 11:05:45 +0530

    createUpdated

--- supplier/src/component/Delivery Management/CreateDelivery.js
+++ supplier/src/component/Delivery Management/CreateDelivery.js
@@ -2,7 +2,6 @@ import React,{useState,  useEffect } from "react";
 import axios from "axios";
 import {useHistory } from "react-router-dom";
 import Header from '../Dashboard/Header/Header';
-import "./style.css"
 
 
 export default function CreateDelivery() {
@@ -22,13 +21,11 @@ export default function CreateDelivery() {
     const [deliveryFee, setDeliveryFee] = useState(0); // Initialize deliveryFee
     const history = useHistory();
 
-
-
     function sendData(a){
-// Calculate delivery fee based on distance
+
       if (distance > 10) {
         const extraDistance = distance - 10;
-        const calculatedDeliveryFee = extraDistance * 20;
+        const calculatedDeliveryFee = extraDistance * 50;
         setDeliveryFee(calculatedDeliveryFee);
     } else {
         setDeliveryFee(0); // Reset deliveryFee if distance is 10km or less
@@ -104,53 +101,10 @@ export default function CreateDelivery() {
     <div>
       <Header />
 
-<div className="container" id="createDeliveryContainer" >
-    <form onSubmit={sendData}>
-      <h2 id="btnAllDelivery">Create New Delivery Records</h2>
+<div className="container" id="createSupplier" >
+    <form onSubmit={sendData} style={{ position: "relative",width: "70%" }}>
+      <h2 id="AllSupplier">Create New Delivery Records</h2>
       <br></br>
-<div className="mb-3">
-<label for="exampleInputEmail1" className="form-label"  id='createDelivery'>Customer Name</label>
-<input type="text" className="form-control" id="exampleInputPassword1" aria-describedby="emailHelp" placeholder="Enter Customer Name" 
- pattern="[A-Za-z\s]+" title="Please enter only letters and spaces"
-
-onChange={(e) =>{
-
-setName(e.target.value);
-}}/>
- {!/^[A-Za-z\s]+$/.test(name) && (
-        <p style={{ color: "red", fontSize: "14px", marginTop: "5px" }}>Please enter only letters and spaces</p>
-    )}
-
-</div>
-
-<div className="mb-3">
-<label for="exampleInputPassword1" className="form-label" id='createDelivery'>Contact Number</label>
-<input type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter Contact Number"
-pattern="^\d{10}$"
-onChange={(e) =>{
-
-setNumber(e.target.value);
-}}/>
-</div>
-
-<div className="row">
-    <div className="col">
-<div className="mb-3">
-<label for="exampleInputPassword1" className="form-label" id='createDelivery'>Order ID</label>
-
-<input type="text" className="form-control" id="exampleInputPassword1" aria-describedby="emailHelp" placeholder="Enter Order ID" 
-onChange={(e) =>{
-
-setOid(e.target.value);
-}}/>
-</div>
-</div>
-
-<div className="col">
-<div className="mb-3">
-<label for="exampleInputPassword1" className="form-label" id='createDelivery'>Delivery Code</label>
-<input type="text" className="form-control" id="dateInput" aria-describedby="emailHelp" placeholder="Enter Delivery Code" 
-
 
       <div className="mb-3">
                         <label htmlFor="exampleInputPassword1" className="form-label" id="supplier">
@@ -204,7 +158,6 @@ setOid(e.target.value);
 <div className="mb-3">
 <label for="exampleInputEmail1" className="form-label" id='supplier'>Delivery Code</label>
 <input type="text" className="form-control" id="exampleInputPassword1" aria-describedby="emailHelp" placeholder="Enter Delivery Code" 
-
 onChange={(e) =>{
 
 setCode(e.target.value);
@@ -214,7 +167,7 @@ setCode(e.target.value);
 </div>
 
 <div className="mb-3">
-<label for="exampleInputPassword1" className="form-label" id='createDelivery'>Address</label>
+<label for="exampleInputPassword1" className="form-label" id='supplier'>Address</label>
 <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter Customer Address"
 onChange={(e) =>{
 
@@ -223,7 +176,7 @@ setAddress(e.target.value);
 </div>
 
 <div className="mb-3">
-    <label htmlFor="dateInput" className="form-label" id='createDelivery' >Date</label>
+    <label htmlFor="dateInput" className="form-label" id='supplier' >Date</label>
     <input type="date" id="dateInput" name="date" max={""} value={date} style={{marginLeft:"3px"}}
     className="form-control"
      onChange={(e) =>
@@ -233,7 +186,7 @@ setAddress(e.target.value);
 
 
 <div className="mb-3">
-<label for="exampleInputPassword1" className="form-label" id='createDelivery'>Additional Information</label>
+<label for="exampleInputPassword1" className="form-label" id='supplier'>Additional Information</label>
 <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter Additional Information"
 onChange={(e) =>{
 
@@ -242,35 +195,28 @@ setNote(e.target.value);
 </div>
 
 <div className="mb-3">
-<label for="exampleInputPassword1" className="form-label" id='createDelivery'>Delivery Status</label>
+<label for="exampleInputPassword1" className="form-label" id='supplier'>Delivery Status</label>
+<input type="text" className="form-control" id="exampleInputPassword1" placeholder="Enter Delivery Status"
+onChange={(e) =>{
 
-<select
-              className="form-select"
-              id="exampleInputPassword1"
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-              required
-            >
-              <option value="">Select Status</option>
-              <option value="Pending">Pending</option>
-              <option value="Delivered">Delivered</option>
-</select>
-
+setStatus(e.target.value);
+}}/>
 </div>
+
 <div className="mb-3">
-  <label htmlFor="exampleInputDistance" className="form-label" id='createDelivery'>Distance (in km)</label>
-  <input type="number" className="form-control" id="exampleInputPassword1" placeholder="Enter Distance"
+  <label htmlFor="exampleInputDistance" className="form-label" id='supplier'>Distance (in km)</label>
+  <input type="number" className="form-control" id="exampleInputDistance" placeholder="Enter Distance"
       onChange={(e) => setDistance(parseInt(e.target.value))} />
 </div>
 
 <div class="count-display" style={{marginBottom:"2%", width:"70%"}}>
-    <label class="count-label" for="exampleInputPassword1" id='createDelivery'>Delivery Fee:</label>
+    <label class="count-label" for="exampleInputPassword1" id='supplier'>Delivery Fee:</label>
     <div class="count-value">LKR: {deliveryFee}</div>
 </div>
 
-<button type="submit" className="btn btn-success" style={{marginTop:"15px",borderRadius:"20px"}}>
+<button type="submit" className="btn btn-success" style={{marginTop:"15px"}}>
 <i className='fas fa-save'></i>
-&nbsp; Save Delivery
+&nbsp; Save
 </button>
 </form>
 </div>
