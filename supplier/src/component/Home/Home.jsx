746c31caecd97686aa5c34afea75cf236ad5257e
Ushan Mihiranga <ushanmihiranga2017@gmail.com>
2024-05-05 11:41:45 +0530

    new

--- supplier/src/component/Home/Home.jsx
+++ supplier/src/component/Home/Home.jsx
@@ -43,7 +43,7 @@ export default function Home() {
                                 <h1 className="display-3 fw-bolder mb-5"><span className="text-gradient d-inline">Get online and grow fast</span></h1>
                                 <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
                                     <a className="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="/customer">
-                                    <i className='fas fa-user'></i>&nbsp;Customer</a>
+                                    <i className='fas fa-user'></i>&nbsp;User</a>
                                     <a className="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="/history">
                                     <i className='fas fa-history'></i>&nbsp;History</a>
                                 </div>
